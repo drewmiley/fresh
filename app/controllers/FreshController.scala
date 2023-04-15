553d11dae483e3b67fed78892f5536e83bdc7054
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-04-15 01:48:16 +0100

    Adds TODO on how to implement

--- app/controllers/FreshController.scala
+++ app/controllers/FreshController.scala
@@ -19,6 +19,16 @@ class FreshController @Inject()(cc: ControllerComponents) extends AbstractContro
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
+//    TODO: How to implement \/
+    //    Load index
+    //    Add scala comet + link
+    //    Create ticker source
+    //    Move wiki scraper into source
+    //    Display wiki scraping using comet for 1 to 5/10 times
+    //    Create pool fixture scraper
+    //    Replace wiki scraper with pool fixture scraper for 1 to 5/10 times
+    //    Make it clever enough to stop at end of fixtures
+    //    Move out constants into env file
     Ok(views.html.fresh.index())
   }
   
