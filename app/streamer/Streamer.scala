5202a33ff28fdaa782771a776e1ae927815a818c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-04-15 17:22:01 +0100

    Creates initial scraper

--- app/streamer/Streamer.scala
+++ app/streamer/Streamer.scala
@@ -1,20 +1,15 @@
 package streamer
 
 import akka.stream.scaladsl.Source
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import scraper.Scraper
 
 trait Streamer {
 
   def wikiSource: Source[String, _] = {
+    val document = Scraper.document
     val iterable = (1 to 10).map(_ => document)
     val vlah = Source(iterable)
     vlah.takeWhile(d => d.hasText).map(_.title())
-  }
-
-  def document: Document = {
-    val doc = Jsoup.connect("http://en.wikipedia.org/").get()
-    doc
   }
 
 }
